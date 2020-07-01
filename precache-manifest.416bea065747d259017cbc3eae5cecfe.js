a83ab446a070da6a001da9f343a2b196947d01b9
unknown <petr.minailyuk@gmail.com>
2020-07-02 02:10:34 +0300

    first

---
+++ precache-manifest.416bea065747d259017cbc3eae5cecfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4e61b627261ddd237a422fd1ee5f8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71be4b2b5b2efd9fcaff",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "afcc24d3dec3137bd865",
+    "url": "/static/css/main.1add3318.chunk.css"
+  },
+  {
+    "revision": "71be4b2b5b2efd9fcaff",
+    "url": "/static/js/2.afce0a38.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.afce0a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcc24d3dec3137bd865",
+    "url": "/static/js/main.487182f8.chunk.js"
+  },
+  {
+    "revision": "305df94d60e70e87fd3e",
+    "url": "/static/js/runtime-main.99a3fe53.js"
+  }
+]);
